dbb52c3c8f877175d0d07c22c01eb9daafc94665
divapermanaputraid-rgb <divapermanaputra.id@gmail.com>
2026-08-16 01:23:30 +0700

    Add Plant model

---
+++ urbanGrow/Data/Models/Plant.swift
@@ -0,0 +1,27 @@
+import SwiftData
+import Foundation
+
+@Model
+class Plant {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var icon: String
+    var colorHex: String
+
+    @Relationship(deleteRule: .cascade, inverse: \Milestone.plant)
+    var milestones: [Milestone]?
+
+    @Relationship(deleteRule: .nullify, inverse: \Batch.plant)
+    var batches: [Batch]?
+
+    init(id: UUID = UUID(), name: String, icon: String, colorHex: String) {
+        self.id = id
+        self.name = name
+        self.icon = icon
+        self.colorHex = colorHex
+    }
+
+    var plantType: PlantType? {
+        PlantType(rawValue: name)
+    }
+}
