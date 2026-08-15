cdb8d6cce10220eeaaaf84e39b68990fe976303e
divapermanaputraid-rgb <divapermanaputra.id@gmail.com>
2026-08-16 01:28:31 +0700

    Add Milestone model

---
+++ urbanGrow/Data/Models/Milestone.swift
@@ -0,0 +1,37 @@
+import SwiftData
+import Foundation
+
+@Model
+class Milestone {
+    var id: UUID
+    var dayOffset: Int
+    var title: String
+    var desc: String
+    var isSlideable: Bool
+    var reminderTime: Date?
+    var order: Int
+
+    @Relationship(inverse: \Plant.milestones)
+    var plant: Plant?
+
+    @Relationship(inverse: \ScheduledTask.milestone)
+    var scheduledTasks: [ScheduledTask]?
+
+    init(
+        id: UUID = UUID(),
+        dayOffset: Int,
+        title: String,
+        desc: String = "",
+        isSlideable: Bool = true,
+        reminderTime: Date? = nil,
+        order: Int = 0
+    ) {
+        self.id = id
+        self.dayOffset = dayOffset
+        self.title = title
+        self.desc = desc
+        self.isSlideable = isSlideable
+        self.reminderTime = reminderTime
+        self.order = order
+    }
+}
